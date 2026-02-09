0d2d01ac455f1c1367b8ee086b8618bfc2f21162
StarMoon <2549007745@qq.com>
2026-02-09 20:30:28 +0800

    Update repository URL in Example.lua

--- Example.lua
+++ Example.lua
@@ -1,4 +1,4 @@
-local repo = "https://raw.githubusercontent.com/StarMoon/Obsidian/main/"
+local repo = "https://raw.githubusercontent.com/StarMoon-SL/Obsidian/main/"
 local Library = loadstring(game:HttpGet(repo .. "Library.lua"))()
 local ThemeManager = loadstring(game:HttpGet(repo .. "addons/ThemeManager.lua"))()
 local SaveManager = loadstring(game:HttpGet(repo .. "addons/SaveManager.lua"))()
